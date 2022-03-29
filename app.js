6a0c1bacb0c15841a74e3e3cf8b524c10551d47c
VladJutnik <v2517v@gmail.com>
2022-03-29 17:27:59 +0700

    css style

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 console.log(1111)
+const list = document.querySelectorAll('.list')
 
-
+function addTask(){
+    const btn = document.querySelector('.add_btn'),
+        addBtn = document.querySelector('.add_item-btn')
+}
