571d4ce70cf5adabb18cf125f8430ac9316d8eb6
VA <v2517v@gmail.com>
2022-03-29 22:06:07 +0700

    css style

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log(1111)
 const list = document.querySelectorAll('.list')
 
 function addTask(){
+    //получаем данные со страницы
     const btn = document.querySelector('.add_btn'),
         addBtn = document.querySelector('.add_item-btn')
 }
