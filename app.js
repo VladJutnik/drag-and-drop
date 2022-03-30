b95db6695fc8f9aa76a4af0815e209bc04b8ccb1
Владислав Андреевич Жутник <56529566+VladJutnik@users.noreply.github.com>
2022-03-30 17:18:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function addTask(){
         btn.style.display = 'block'
     })
     addBtn.addEventListener('click', ()=>{
-       const newItems = document.createElement('div')
+        const newItems = document.createElement('div')
         newItems.classList.add('list_item')
         newItems.draggable = true
         newItems.textContent = value
@@ -42,6 +42,7 @@ function addTask(){
         value = ''
         form.style.display = 'none'
         btn.style.display = 'block'
+        dragAndDrop()
     })
 }
 addTask()
@@ -50,9 +51,12 @@ function addBoard(){
     const boards = document.querySelector('.boards')
     const board = document.createElement('div')
     board.classList.add('boards_items')
-    board.innerHTML = ` <span contenteditable="true" class="title">Название</span>`
+    board.innerHTML = ` 
+    <span contenteditable="true" class="title">Название</span>
+    <div class="list"></div>`
     boards.append(board)
     changeTitle()
+    dragAndDrop()
 }
 button.addEventListener('click',addBoard)
 //добавление доски
@@ -65,3 +69,54 @@ function changeTitle(){
     })
 }
 changeTitle()
+//перетаскивание
+let dragItem = null //тот элемент который будем перетаскивать
+/*let list = null //тот элемент который будем перетаскивать*/
+function dragAndDrop(){
+    const listItems = document.querySelectorAll('.list_item'),
+        lists2 = document.querySelectorAll('.list')
+    //перебераем массивы
+    for(let i = 0; i < listItems.length; i++){
+        const item = listItems[i]
+        //начали перемещать элемент
+        item.addEventListener('dragstart', ()=>{
+            dragItem = item
+            setTimeout(()=>{
+                //при перетаскивание мы скрываем блок а нужно его удалять!
+                item.style.display = 'none'
+            }, 0.2)
+        })
+        //Надо сделать так при перетаскивании не удалять элемент сразу а только после того как он dragend совершил
+        //закончили перемещать элемент
+        item.addEventListener('dragend', ()=>{
+            setTimeout(()=>{
+                item.style.display = 'block'
+                dragItem = null
+            }, 0.2)
+        })
+        //удаление элемента
+        item.addEventListener('dblclick', ()=>{
+            item.remove()
+        })
+
+        for(let j = 0; j < lists2.length; j++){
+            const list555 = lists2[j]
+            //перетакивание на новую доску
+            list555.addEventListener('dragover', e =>{
+                e.preventDefault()
+            })
+            list555.addEventListener('dragenter', function (e){
+                e.preventDefault() //убираем стандартные работы браузера
+                this.style.backgroundColor = 'rgba(0,0,0,.3)'
+            })
+            list555.addEventListener('dragleave', function (e){
+                this.style.backgroundColor = 'rgba(0,0,0,0)'
+            })
+            list555.addEventListener('drop', function (e){
+                this.style.backgroundColor = 'rgba(0,0,0,0)'
+                this.append(dragItem)
+            })
+        }
+    }
+}
+dragAndDrop()
